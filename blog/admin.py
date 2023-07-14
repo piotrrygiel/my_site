064a63529dcf7c34db914995aae7f2fa8cb33e5f
ryglu <rygielpiotr18@gmail.com>
2023-07-14 17:25:44 +0200

    Added forms, sessions and images handling.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Post, Author, Tag
-
-# Register your models here.
+from .models import Post, Author, Tag, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,6 +8,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "post")
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
