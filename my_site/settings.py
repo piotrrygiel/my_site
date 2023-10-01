ba63296612d6408dc153c0b0b096dff7f34ed6cb
ryglu <rygielpiotr18@gmail.com>
2023-10-01 16:41:45 +0200

    Edited settings.py to serve media files using Amazon S3

--- my_site/settings.py
+++ my_site/settings.py
@@ -134,7 +134,20 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-MEDIA_ROOT = BASE_DIR / "uploads"
-MEDIA_URL = "/files/"
+# MEDIA_ROOT = BASE_DIR / "uploads"
+# MEDIA_URL = "/files/"
 
 DJANGO_SUPERUSER_PASSWORD = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "default_password")
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = 'django-media-files'
+AWS_S3_REGION_NAME = 'eu-central-1'
+
+AWS_QUERYSTRING_AUTH = False
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/files/'
