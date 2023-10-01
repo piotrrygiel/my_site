faf1349e96ca623655d1227c4c7d1847f83a3ef7
ryglu <rygielpiotr18@gmail.com>
2023-10-01 19:56:00 +0200

    Corrected Amazon S3 configuration

--- my_site/settings.py
+++ my_site/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'blog'
+    'blog',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -144,10 +145,16 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = 'django-media-files'
 AWS_S3_REGION_NAME = 'eu-central-1'
 
-AWS_QUERYSTRING_AUTH = False
+# AWS_QUERYSTRING_AUTH = False
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_FOLDER = 'static'
+MEDIAFILES_FOLDER = 'files'
 
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/files/'
+STATICFILES_STORAGE = 'custom_storages.StaticFileStorage'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaFileStorage'
+
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/files/'
